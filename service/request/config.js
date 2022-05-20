106cbc346ec895c0b48ca07d756fb886f7ff409a
August-Rushme <864011713@qq.com>
2022-05-20 22:48:19 +0800

    feat: 优化界面

--- service/request/config.js
+++ service/request/config.js
@@ -1,8 +1,8 @@
 //congif.js
 const config = {
-  // baseUrl: 'http://127.0.0.1:8081', 
+  baseUrl: 'http://127.0.0.1:8081', 
 
-  baseUrl: 'http://150.158.98.218:8081', //请求的基本路径
+  // baseUrl: 'http://150.158.98.218:8081', //请求的基本路径
   modalColor: '#5271FF', //弹窗颜色 
 }
 
