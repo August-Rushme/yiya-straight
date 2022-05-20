861e546ebc05edcd3ec5937b3be017f7c573fb33
Kenny2019 <2435587095@qq.com>
2022-05-20 22:32:49 +0800

    新增登录特性

--- pages/test/test.vue
+++ pages/test/test.vue
@@ -4,13 +4,17 @@
 <view class="container">
 	<view :class="['loginBtn',active ? 'active' : '']" @click="login">
 		{{loginText}}
-		<template v-if="active">
+<!-- 		<template v-if="active">
 		<view class="loadingIcon" :class="active ? 'rotation' : '' ">  
 		</view>
-		</template>
-         <view class="success">
-         	
-         </view>
+		</template> -->
+		<view class="success">
+			<view class="successHeight">
+			</view>
+			<view class="successWidth">
+			</view>
+		</view>
+ 
 	</view>
 </view>
 </template>
@@ -65,10 +69,43 @@ export default {
          border-bottom-color: white;
       }
 	  .success {
-		  width: 30px;
-		  height: 40px;
+		  position: relative;
+		  width: 20px;
+		  height: 35px;
 		  transform: rotate(45deg);
-		  
+	  }
+	  .successHeight{
+		  position: absolute;
+		  right: 0;
+		  bottom: 0;
+		  width: 5px;
+		  height: 0px;
+		  background-color: white;
+	  }
+	  .sucessAnimation {
+		  animation: beHeight .5s cubic-bezier(.2,1,0,1) forwards .3s;
+	  }
+	  .successWidthAnimation{
+		  animation: beWidth .3s cubic-bezier(.2,1,0,1) forwards;
+	  }
+	  .successWidth {
+		  position: absolute;
+		  left: 0;
+		  bottom: 0;
+		  width: 0px;
+		  height: 5px;
+		  background-color: white;
+	  }
+	  @keyframes beHeight {
+	  	to {
+			height: 35px;
+		
+		}
+	  }
+	  @keyframes beWidth {
+	  	to {
+			 width: 20px;
+		}
 	  }
   }
 }
