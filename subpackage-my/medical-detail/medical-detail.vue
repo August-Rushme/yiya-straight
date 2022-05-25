8768e46df7ba4551f1fe3155cb6caa04e9a31216
Kenny2019 <2435587095@qq.com>
2022-05-25 14:07:25 +0800

    开发了病例详情页

--- subpackage-my/medical-detail/medical-detail.vue
+++ subpackage-my/medical-detail/medical-detail.vue
@@ -1,6 +1,29 @@
 <template>
-	<view>
-		病例详情页
+	<view class="page">
+		<!-- 标题 -->
+         <view class="p-2" style="background-color: white;">
+         	<view class="d-flex j-sb py-1 border-bottom">
+         		<text>医生</text>
+				<text>李满元</text>
+         	</view>
+			<view class="d-flex j-sb py-1">
+				<text>就诊时间</text>
+				<text>2022-05-22 14:45:00</text>
+			</view>
+         </view>	
+		 <!-- 病例详情 -->
+		 <view class="mt-2 p-2" style="background-color: white;">
+                       <view class="border-bottom">
+                            <view class="py-1">
+                              口腔检查
+                            </view>           
+							<view class="imgCard">
+								<view class="img mt-3">
+									
+								</view>
+							</view>
+                       </view>  		 	
+		 </view>
 	</view>
 </template>
 
@@ -17,6 +40,14 @@
 	}
 </script>
 
-<style>
+<style scoped lang="scss">
+	.page{
+		min-height: 100vh;
+		background-color: #f4f4f4;
+		.imgCard{
+			border: 2rpx solid #f4f4f4;
+			border-radius: 15rpx;
+		}
+	}
 
 </style>
